7b6500f792d27d8f0c90c04c3a41f237f86c70e7
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-30 12:18:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -108,26 +108,30 @@ class ShoppingCart {
     this.taxRate = 8.25;
   }
 
-addItem(id, products) {
-  const product = products.find((item) => item.id === id);
-  const { name, price } = product;
-  this.items.push(product);
+  addItem(id, products) {
+    const product = products.find((item) => item.id === id);
+    const { name, price } = product;
+    this.items.push(product);
 
-  const totalCountPerProduct = {};
-  this.items.forEach((dessert) => {
-    totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
-  });
+    const totalCountPerProduct = {};
+    this.items.forEach((dessert) => {
+      totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
+    })
 
-  const currentProductCount = totalCountPerProduct[product.id];
-  const currentProductCountSpan = document.getElementById(`product-count-for-id${id}`);
+    const currentProductCount = totalCountPerProduct[product.id];
+    const currentProductCountSpan = document.getElementById(`product-count-for-id${id}`);
 
-  currentProductCount > 1 
-    ? currentProductCountSpan.textContent = `${currentProductCount}x`
-    : productsContainer.innerHTML += `
+    currentProductCount > 1 
+      ? currentProductCountSpan.textContent = `${currentProductCount}x`
+      : productsContainer.innerHTML += `
       <div id="dessert${id}" class="product">
-        <p><span class="product-count" id="product-count-for-id${id}"></span> ${name}</p>
+        <p>
+          <span class="product-count" id="product-count-for-id${id}"></span>${name}
+        </p>
         <p>${price}</p>
       </div>
-    `;
-}
+      `;
+  }
 };
+
+const cart = new ShoppingCart();
