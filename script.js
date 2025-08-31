722c77e164384f98736648b82d5e73b836e85355
Aleksandra Mate <matealeksandra@gmail.com>
2025-08-31 18:12:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -141,14 +141,13 @@ class ShoppingCart {
 const cart = new ShoppingCart();
 const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
 
-[...addToCartBtns].forEach(
-  (btn) => {
-    btn.addEventListener("click", (event) => {
-      cart.addItem(Number(event.target.id), products);
+[...addToCartBtns].forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    cart.addItem(Number(event.target.id), products);
+    totalNumberOfItems.textContent = cart.getCounts();
+  });
+});
 
-    })
-  }
-);
 
 cartBtn.addEventListener("click", () => {
   isCartShowing = !isCartShowing;
