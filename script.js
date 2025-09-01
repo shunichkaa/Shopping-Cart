94b18f5ff11e6128c84d025b9a67cfa21a523b1d
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-01 12:18:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -192,3 +192,4 @@ cartBtn.addEventListener("click", () => {
   cartContainer.style.display = isCartShowing ? "block" : "none";
 });
 
+clearCartBtn.addEventListener("click", cart.clearCart.bind(cart));
