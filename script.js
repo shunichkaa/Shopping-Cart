91b6bc023eebc159224073ba4bdeb545f6bad065
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-01 12:10:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -141,17 +141,15 @@ class ShoppingCart {
     return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
   }
 
-calculateTotal() {
-  const subTotal = this.items.reduce((total, item) => total + item.price, 0);
-  const tax = this.calculateTaxes(subTotal);
-  this.total = subTotal + tax;
-
-  cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
-  cartTaxes.textContent = `$${tax.toFixed(2)}`;
-  cartTotal.textContent = `$${this.total.toFixed(2)}`;
-
-  return this.total;
-}
+  calculateTotal() {
+    const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+    const tax = this.calculateTaxes(subTotal);
+    this.total = subTotal + tax;
+    cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+    cartTaxes.textContent = `$${tax.toFixed(2)}`;
+    cartTotal.textContent = `$${this.total.toFixed(2)}`;
+    return this.total;
+  }
 };
 
 const cart = new ShoppingCart();
@@ -162,7 +160,8 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
     btn.addEventListener("click", (event) => {
       cart.addItem(Number(event.target.id), products);
       totalNumberOfItems.textContent = cart.getCounts();
-    })
+      cart.calculateTotal();
+    });
   }
 );
 
